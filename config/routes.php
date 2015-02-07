2dc8251ad53732f004d3418b34105ea03d4ae026
zoghal <saleh.souzanchi@gmail.com>
2015-02-07 22:47:20 +0330

    fix

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 use Cake\Routing\Router;
 
 Router::prefix('admin', function($routes){
-    $routes->plugin('Accunting', [ 'path' => '/accunting'], function($routes) {
+    $routes->plugin('Rita/Accunting', [ 'path' => '/accunting'], function($routes) {
         $routes->redirect('/', ['controller' => 'Dashboard','action' => 'index']);
         $routes->fallbacks('InflectedRoute');
         
@@ -18,8 +18,8 @@ Router::scope('/', function ($routes) {
 Router::prefix('client', function($routes)
 {
      
-  $routes->plugin('Accunting', [ 'path' => '/accunting'], function($routes) {
-        
+  $routes->plugin('Rita/Accunting', [ 'path' => '/accunting'], function($routes) {
+             $routes->connect('/', ['controller' => 'reports','action' => 'index']);
         $routes->fallbacks('InflectedRoute');
         
     });
